2da596f3ab3217ba3f0c5a603a7c3ef548e6e4c8
Lawrence Tran <lawrence@tran.com>
2016-04-02 00:24:44 -0500

    Installed PocketSVG for example.

---
+++ Example/Pods/Target Support Files/PocketSVG/PocketSVG-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "PocketSVG.h"
+
+FOUNDATION_EXPORT double PocketSVGVersionNumber;
+FOUNDATION_EXPORT const unsigned char PocketSVGVersionString[];
+
--- Example/Pods/Target Support Files/Pods-ColorMaskingButton_Example/Pods-ColorMaskingButton_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-ColorMaskingButton_Example/Pods-ColorMaskingButton_Example-frameworks.sh
@@ -85,7 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-ColorMaskingButton_Example/ColorMaskingButton.framework"
+  install_framework "Pods-ColorMaskingButton_Example/PocketSVG.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-ColorMaskingButton_Example/ColorMaskingButton.framework"
+  install_framework "Pods-ColorMaskingButton_Example/PocketSVG.framework"
 fi
