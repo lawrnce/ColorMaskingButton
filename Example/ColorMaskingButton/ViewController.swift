1c20fd8c648f83de5759b3f05f0c2740b5d0270c
Lawrence Tran <lawrence@tran.com>
2016-04-02 01:05:42 -0500

    Added UI elements to example.

--- Example/ColorMaskingButton/ViewController.swift
+++ Example/ColorMaskingButton/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
 //         let path = PocketSVG.pathFromSVGFileNamed(SVGname).takeUnretainedValue()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        setupLeftToRight()
     }
 
     override func prefersStatusBarHidden() -> Bool {
@@ -25,6 +34,44 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // Setup each example
+    private func setupLeftToRight() {
+        self.label.text = "Left to Right"
+        self.slider.setValue(0.0, animated: false)
+    }
+    
+    private func setupRightToLeft() {
+        self.label.text = "Right to Left"
+        self.slider.setValue(0.0, animated: false)
+    }
+    
+    private func setupTopToBottom() {
+        self.label.text = "Top to Bottom"
+        self.slider.setValue(0.0, animated: false)
+    }
+    
+    private func setupBottomToTop() {
+        self.label.text = "Bottom to Top"
+        self.slider.setValue(0.0, animated: false)
+    }
 
+    @IBAction func segmentedControlValueChanged(sender: UISegmentedControl) {
+        let value = sender.selectedSegmentIndex
+        
+        switch (value) {
+        case 0:
+            setupLeftToRight()
+        case 1:
+            setupRightToLeft()
+        case 2:
+            setupTopToBottom()
+        case 3:
+            setupBottomToTop()
+            
+        default:
+            break
+        }
+    }
 }
 
