0d567353a36591bfbc50fd9cb14ba992c3fc9988
Lawrence Tran <lawrence.c.tran@outlook.com>
2017-06-22 09:22:51 -0700

    Update Podfile

--- Example/Pods/Target Support Files/Pods-ColorMaskingButton_Tests/Pods-ColorMaskingButton_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_ColorMaskingButton_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ColorMaskingButton_TestsVersionString[];
-
